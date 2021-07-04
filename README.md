6a1703f245af12426ce712f8716b6a5d6266ddde
Renan Stellet <74331129+rstellet8@users.noreply.github.com>
2021-07-04 13:07:02 -0300

    @13:06

--- README.md
+++ README.md
@@ -5,5 +5,6 @@
 fonte dos dados: <https://www.kaggle.com/wordsforthewise/lending-club>
 
 ## Conteúdo
+
 - sepAmostra: Extrai uma amostra de 10000 linhas dos dados originais
 - featureDiscovery: Limpeza, transformação, criação de novos features
--- sepAmostra.py
+++ sepAmostra.py
@@ -7,7 +7,7 @@ def sepAmostra(pathRaw, pathToSave, randomState=None, nSample=10000):
 
     df = pd.read_csv(pathRaw)
     df = df.sample(n=nSample, random_state=randomState)
-    df.to_csv(pathToSave)
+    df.to_csv(pathToSave, index=False)
     del df
 
 
@@ -23,12 +23,12 @@ if __name__ == "__main__":
         pathRaw="data/raw/accepted_2007_to_2018Q4.csv",
         pathToSave="data/sample/sampleAccepted.csv",
         nSample=10000,
-        randomState=0
+        randomState=None
     )
 
     sepAmostra(
         pathRaw="data/raw/rejected_2007_to_2018Q4.csv",
         pathToSave="data/sample/sampleRejected.csv",
         nSample=10000,
-        randomState=0
+        randomState=None
     )
